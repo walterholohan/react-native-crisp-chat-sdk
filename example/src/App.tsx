4aa3228d1b0a90a9ce5378e40df799c0c63ab063
Walter Holohan <walter.holohan@11fs.com>
2020-05-19 09:23:12 +0100

    fix lint issue

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { CrispChatSDK, CrispChatUI } from 'react-native-crisp-chat-sdk';
