3ca8c0557804add2c984d76e7b3443847f83ab00
Baptiste Jamin <baptiste@crisp.chat>
2025-01-10 10:34:30 +0100

    chore: update android package

--- plugin/src/withCrispChat.ts
+++ plugin/src/withCrispChat.ts
@@ -107,13 +107,13 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
 }
 
 export function setGradleCrispDependency(buildGradle: string) {
-  const crispDependency = /implementation 'im.crisp:crisp-sdk:2.0.5'/g;
+  const crispDependency = /implementation 'im.crisp:crisp-sdk:2.0.9'/g;
   let result = buildGradle;
   if (!result.match(crispDependency)) {
     result = result.replace(
       /dependencies\s?{/,
       `dependencies {
-    implementation 'im.crisp:crisp-sdk:2.0.5'`
+    implementation 'im.crisp:crisp-sdk:2.0.9'`
     );
   }
 
