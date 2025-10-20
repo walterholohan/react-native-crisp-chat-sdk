4319211b719c75f4978431c22f99ef4a93fdfa5b
Armand Petit <armand_petit@outlook.fr>
2025-10-20 16:11:22 +0200

    refactor: enhance CrispChatSdk by making methods public and implementing dedicated parsers

--- ios/CrispChatSdk.swift
+++ ios/CrispChatSdk.swift
@@ -1,102 +1,74 @@
 import Crisp
 
 @objc(CrispChatSdk)
-class CrispChatSdk: NSObject {
+public class CrispChatSdk: NSObject {
 
     @objc
-    func configure(_ websiteId: String) {
+    public func configure(_ websiteId: String) {
         CrispSDK.configure(websiteID: websiteId)
     }
 
     @objc
-    func setTokenId(_ tokenID: String?) {
+    public func setTokenId(_ tokenID: String?) {
         CrispSDK.setTokenID(tokenID: tokenID)
     }
 
-     @objc
-    func setUserEmail(_ email: String, signature: String?) {
+    @objc
+    public func setUserEmail(_ email: String, signature: String?) {
         CrispSDK.user.email = email
         CrispSDK.user.signature = signature
     }
 
     @objc
-    func setUserNickname(_ nickname: String) {
+    public func setUserNickname(_ nickname: String) {
         CrispSDK.user.nickname = nickname
     }
+
     @objc
-    func setUserPhone(_ phone: String) {
+    public func setUserPhone(_ phone: String) {
         CrispSDK.user.phone = phone
     }
 
     @objc
-    func setUserCompany(_ companyData: [String: Any]) {
-        let companyName: String? = companyData["name"] as? String
-        
-        var companyUrl: URL? = nil
-        if let urlString: String = companyData["url"] as? String, !urlString.isEmpty {
-            companyUrl = URL(string: urlString)
-        }
-        
-        let companyDescription: String? = companyData["companyDescription"] as? String
-        
-        var employment: Employment? = nil
-        if let employmentData: [String : Any] = companyData["employment"] as? [String: Any] {
-            let title: String? = employmentData["title"] as? String
-            let role: String? = employmentData["role"] as? String
-            employment = Employment(title: title, role: role)
-        }
-        
-        var geolocation: Geolocation? = nil
-        if let geolocationData: [String : Any] = companyData["geolocation"] as? [String: Any] {
-            let city: String? = geolocationData["city"] as? String
-            let country: String? = geolocationData["country"] as? String
-            geolocation = Geolocation(city: city, country: country)
-        }
-        
-        let company = Company(
-            name: companyName,
-            url: companyUrl,
-            companyDescription: companyDescription,
-            employment: employment,
-            geolocation: geolocation
-        )
-        
+    public func setUserCompany(_ companyData: [String: Any]) {
+        let companyDict = companyData as NSDictionary
+        let company = CompanyParser.fromDictionary(companyDict)
         CrispSDK.user.company = company
     }
 
     @objc
-    func setUserAvatar(_ url: String) {
+    public func setUserAvatar(_ url: String) {
         CrispSDK.user.avatar = URL(string: url)
     }
 
     @objc
-    func setSessionSegment(_ segment: String) {
+    public func setSessionSegment(_ segment: String) {
         CrispSDK.session.segment = segment
     }
 
     @objc
-    func setSessionSegments(_ segments: NSArray, overwrite: Bool) {
+    public func setSessionSegments(_ segments: NSArray, overwrite: Bool) {
         let segmentsArray = segments.compactMap { $0 as? String }
         CrispSDK.session.setSegments(segmentsArray, overwrite: overwrite)
     }
 
     @objc
-    func setSessionString(_ key: String, value: String) {
+    public func setSessionString(_ key: String, value: String) {
         CrispSDK.session.setString(value, forKey: key)
     }
 
     @objc
-    func setSessionBool(_ key: String, value: Bool) {
+    public func setSessionBool(_ key: String, value: Bool) {
         CrispSDK.session.setBool(value, forKey: key)
     }
 
     @objc
-    func setSessionInt(_ key: String, value: Int) {
-        CrispSDK.session.setInt(value, forKey: key)
+    public func setSessionInt(_ key: String, value: Double) {
+        CrispSDK.session.setInt(Int(value), forKey: key)
     }
 
     @objc
-    func getSessionIdentifier(
+    public func getSessionIdentifier(
         _ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock
     ) {
         let sessionIdentifier = CrispSDK.session.identifier
@@ -108,13 +80,12 @@ class CrispChatSdk: NSObject {
     }
 
     @objc
-    func pushSessionEvent(_ eventName: String, color: NSInteger) {
-        let sessionEventColor = convertIntegerToColor(color)
-        CrispSDK.session.pushEvent(Crisp.SessionEvent(name: eventName, color: sessionEventColor))
+    public func pushSessionEvent(_ eventName: String, color: Crisp.SessionEventColor) {
+        CrispSDK.session.pushEvent(Crisp.SessionEvent(name: eventName, color: color))
     }
 
     @objc
-    func pushSessionEvents(_ events: NSArray) {
+    public func pushSessionEvents(_ events: NSArray) {
         var sessionEvents: [Crisp.SessionEvent] = []
 
         for event in events {
@@ -145,14 +116,13 @@ class CrispChatSdk: NSObject {
         }
     }
 
-
     @objc
-    func resetSession() {
+    public func resetSession() {
         CrispSDK.session.reset()
     }
 
     @objc
-    func show() {
+    public func show() {
         DispatchQueue.main.async {
             var viewController = RCTPresentedViewController()
 
@@ -165,17 +135,17 @@ class CrispChatSdk: NSObject {
     }
 
     @objc
-    func searchHelpdesk() {
+    public func searchHelpdesk() {
         CrispSDK.searchHelpdesk()
     }
 
     @objc
-    func openHelpdeskArticle(_ id: String, locale: String, title: String?, category: String?) {
+    public func openHelpdeskArticle(_ id: String, locale: String, title: String?, category: String?) {
         CrispSDK.openHelpdeskArticle(locale: locale, slug: id, title: title, category: category)
     }
     
     @objc
-    func runBotScenario(_ id: String) {
+    public func runBotScenario(_ id: String) {
         CrispSDK.session.runBotScenario(id: id)
     }
 
