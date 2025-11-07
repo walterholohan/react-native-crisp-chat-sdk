68faae74af5c963dd59e93d5cf8210c51e3c9912
Armand Petit <armand_petit@outlook.fr>
2025-11-07 17:41:21 +0100

    refactor: simplify Metro configuration by integrating with react-native-monorepo-config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,45 +1,16 @@
 const path = require('path');
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const blacklist = require('metro-config/src/defaults/exclusionList');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { withMetroConfig } = require('react-native-monorepo-config');
 
 const root = path.resolve(__dirname, '..');
 
-const defaultConfig = getDefaultConfig(__dirname);
-const {
-  resolver: {sourceExts, assetExts},
-} = defaultConfig;
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
-
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
-    ),
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-
-    assetExts: assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
-  },
-  watchFolders: [path.resolve(__dirname, '../')],
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+  root,
+  dirname: __dirname,
+});
